ed6a0176b4c0454f2f2304b4409984fe12320847
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-03-19 13:42:24 +0100

    Added field _resources and a class Resource to avoid errors

--- MicheleCiavatti/Model/Player.cs
+++ MicheleCiavatti/Model/Player.cs
@@ -1,6 +1,9 @@
 ﻿namespace Model {
 
+    ///This class is a translate of PlayerImpl in the main project
     public class Player {
+
+        private readonly Dictionary<Resource, int> _resources;
 
         public static void Main() {
             Console.WriteLine("Hello");
---
+++ MicheleCiavatti/Model/Resource.cs
@@ -0,0 +1,6 @@
+namespace Model
+{
+    internal class Resource
+    {
+    }
+}
