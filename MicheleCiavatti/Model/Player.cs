37ecd4f47df572ad67b853b4c39b65870d66fda1
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-03-19 13:48:58 +0100

    Setup contructor in Player.cs

--- MicheleCiavatti/Model/Player.cs
+++ MicheleCiavatti/Model/Player.cs
@@ -1,9 +1,13 @@
 ﻿namespace Model {
 
-    ///This class is a translate of PlayerImpl in the main project
+    ///This class is a translation of PlayerImpl in the main project
     public class Player {
 
         private readonly Dictionary<Resource, int> _resources;
+
+        public Player() {
+            _resources = new Dictionary<Resource, int>();
+        }
 
         public static void Main() {
             Console.WriteLine("Hello");
