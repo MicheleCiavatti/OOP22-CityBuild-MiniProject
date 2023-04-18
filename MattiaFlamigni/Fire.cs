e21f581579d05b195f40ad7829952ce87862a5b3
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-04-18 13:21:38 -0700

    update interface Fire

--- MattiaFlamigni/Fire.cs
+++ MattiaFlamigni/Fire.cs
@@ -1,11 +1,37 @@
+// An interface that defines methods for a fire.
+public interface Fire {
+    /**
+     * Calculates the intensity of the fire.
+     *
+     * @return The intensity of the fire.
+     * 
+     * @throws ArgumentException if the calculation fails.
+     */
+    //public int calculateIntensity() throws ArgumentException;
 
-public interface Fire
-{
-    void setcost(int cost);
+    /**
+     * Sets the cost of the fire based on the number of citizens and water resources
+     * in the city.
+     */
+    void setCost();
 
-    void spendgold(int gold);
+    /**
+     * Spends the given amount of gold by the player.
+     *
+     * @param player The player who is spending the gold.
+     * @param cost   The amount of gold to be spent.
+     */
+    void spendGold(Player player, int cost);
 
+    /**
+     * Performs the fire action.
+     */
     void performFireAction();
 
-    int getcost();
+    /**
+     * Returns the cost of the fire.
+     *
+     * @return The cost of the fire.
+     */
+    int getCost();
 }
