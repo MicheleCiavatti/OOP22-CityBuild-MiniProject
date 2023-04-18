f324171af92ffeabff7c05fc48613591eb89ae39
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-04-18 12:56:25 -0700

    implemented fireImpl

---
+++ MattiaFlamigni/FireImpl.cs
@@ -0,0 +1,22 @@
+//classe
+public class FireImpl{
+    //set cost
+    public void setCost() { 
+        this.citizen = city.getCitizens(); 
+        int water = this.getNumResource(Resource.WATER); 
+        int cost = calculateCost(citizen, water);
+        this.cost = cost < MIN_COST ? MIN_COST : cost; 
+    }
+
+    public void spendGold() {
+        player.spendResources(Map.of(Resource.GOLD, cost));
+    }
+
+    public void performFireAction() { 
+        this.setCost(); 
+        this.spendGold();
+        this.destroyBuildings(); 
+    }
+
+    public int getCost() { return cost; }
+}
