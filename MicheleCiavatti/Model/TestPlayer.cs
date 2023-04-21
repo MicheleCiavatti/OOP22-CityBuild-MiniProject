2648b792ebcf07ef014fe3a8a0720d0ff1a36894
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-04-21 21:54:10 +0200

    Done TestCreation() in TestPlayer

--- MicheleCiavatti/Model/TestPlayer.cs
+++ MicheleCiavatti/Model/TestPlayer.cs
@@ -10,6 +10,12 @@ namespace Model
         {
             var p = new Player();
             Assert.NotNull(p);
+            foreach (int r in p.Resources.Values) 
+                Assert.Equal(0, r);
+            var keys = p.Resources.Keys;
+            var resources = Enum.GetValues(typeof(Resource));
+            foreach (Resource r in resources)
+                Assert.True(keys.Contains(r));
         }
     }
 }
